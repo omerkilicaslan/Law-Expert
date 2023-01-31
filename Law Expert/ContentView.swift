a8971aa3c8f3c66bd4361bdb15f3241b3c48daaf
omerkilicaslan <omerrr_2001@hotmail.com>
2023-02-01 01:54:01 +0300

    Initial Commit

---
+++ Law Expert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Law Expert
+//
+//  Created by Ömer Faruk Kılıçaslan on 1.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Law Expert/Law_ExpertApp.swift
@@ -0,0 +1,17 @@
+//
+//  Law_ExpertApp.swift
+//  Law Expert
+//
+//  Created by Ömer Faruk Kılıçaslan on 1.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Law_ExpertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
